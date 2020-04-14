7a9f26da9b6cff2657d0d5ee85a59e8a5d433099
jumpwmk <methus159357@gmail.com>
2020-04-14 22:52:05 +0700

    change backend url

--- src/utils/withBackendUrl.js
+++ src/utils/withBackendUrl.js
@@ -1,5 +1,5 @@
 export const withBackendUrl = (url) => {
   // console.log('url: http://54.169.46.149:3001' + url);
-  console.log('url: http://54.169.46.149:3001' + url);
-  return 'http://54.169.46.149:3001' + url;
+  console.log('url: https://robolog1412.com' + url);
+  return 'https://robolog1412.com' + url;
 };
